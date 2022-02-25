15b88501dd4edf8b8aa18273d6c2121a77270c7c
Andrew Robles <andrewrobles@verizon.net>
2022-02-25 05:36:13 -0800

    Use npm start for docker client command

--- client/src/App.js
+++ client/src/App.js
@@ -25,24 +25,24 @@ function App() {
   }
 
   return (
-    <div className="w-full max-w-lg font-['Inter']">
+    <div className="w-full max-w-md font-['Inter']">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-3">
-          <label className="block text-gray-500 text-lg" for="username">
+          <label className="block text-gray-500 text-md" for="username">
             Username
           </label>
           <input className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text"/>
-          <p className="text-gray-400 text-md">Instagram username, phone number or email address</p>
+          <p className="text-gray-400 text-sm">Instagram username, phone number or email address</p>
         </div>
         <div className="mb-5">
-          <label className="block text-gray-500 text-lg" for="password">
+          <label className="block text-gray-500 text-md" for="password">
             Password
           </label>
           <input className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password"/>
-          <p className="text-gray-400 text-md">Use the password you use to sign into Instagram</p>
+          <p className="text-gray-400 text-sm">Use the password you use to sign into Instagram</p>
         </div>
         <div className="flex items-center justify-between">
-          <button className="bg-[#22AFFF] hover:bg-[#198ED0] text-white text-lg font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline" type="button">
+          <button className="bg-[#22AFFF] hover:bg-[#198ED0] text-white text-md font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline" type="button">
             Log In
           </button>
         </div>
