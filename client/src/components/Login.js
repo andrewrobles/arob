bf3338f6f05f30e43fbc3fc18b4ed9974ce27946
Andrew Robles <andrewrobles@verizon.net>
2022-03-01 07:23:09 -0800

    Revamp login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,27 +1,25 @@
 import '../index.css';
 
 export const Login = () => {
-    return <div className="w-full max-w-md font-['Inter']">
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div className="mb-3">
-        <label className="block text-gray-700 text-md" for="username">
-          Username
-        </label>
-        <input className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text"/>
-        <p className="text-gray-400 text-sm">Instagram username, phone number or email address</p>
-      </div>
-      <div className="mb-5">
-        <label className="block text-gray-700 text-md" for="password">
-          Password
-        </label>
-        <input className="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password"/>
-        <p className="text-gray-400 text-sm">Use the password you use to sign into Instagram</p>
-      </div>
-      <div className="flex items-center justify-between">
-        <button className="bg-[#22AFFF] hover:bg-[#198ED0] text-white text-md font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline" type="button">
-          Log In
-        </button>
-      </div>
-    </form>
-  </div>
+return <div className="font-['Inter'] w-full max-w-xs">
+  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"/>
+    </div>
+    <div className="flex items-center justify-between">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+      </button>
+    </div>
+  </form>
+</div>
 }
