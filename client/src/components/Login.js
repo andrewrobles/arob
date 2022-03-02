7f9bf10c8e26072b1d859c393ee1d8eb88fb95e4
Andrew Robles <andrewrobles@verizon.net>
2022-03-02 07:23:39 -0800

    Map form inputs to login credentials

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,38 +1,49 @@
+import {useState} from 'react'
 import '../index.css';
 
 export const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-return <div className="font-['Inter'] w-full max-w-xs">
-  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
-      </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
-    </div>
-    <div className="mb-6">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
-      </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"/>
-    </div>
-    <div className="flex items-center justify-between">
-      <button onClick={loginButtonAction} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline" type="button">
-        Sign In
-      </button>
-    </div>
-  </form>
-</div>
-}
+  const loginButtonAction = () => {
+    fetch('http://localhost:8000/api/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({'username': username, 'password': password})
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
 
-const loginButtonAction = () => {
-  fetch('http://localhost:8000/api/login/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({'username': 'johndoe', 'password': 'testpassword'})
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
+  return <div className="font-['Inter'] w-full max-w-xs">
+    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+          Username
+        </label>
+        <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username} 
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+          Password
+        </label>
+        <input 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button onClick={loginButtonAction} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline" type="button">
+          Sign In
+        </button>
+      </div>
+    </form>
+  </div>
 }
