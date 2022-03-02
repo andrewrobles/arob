69ebe918be39ea17441806f27de72c84d2b22c00
Andrew Robles <andrewrobles@verizon.net>
2022-03-02 06:52:28 -0800

    Create login button action

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import '../index.css';
 
 export const Login = () => {
+
 return <div className="font-['Inter'] w-full max-w-xs">
   <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div className="mb-4">
@@ -16,10 +17,22 @@ return <div className="font-['Inter'] w-full max-w-xs">
       <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"/>
     </div>
     <div className="flex items-center justify-between">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline" type="button">
+      <button onClick={loginButtonAction} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline" type="button">
         Sign In
       </button>
     </div>
   </form>
 </div>
+}
+
+const loginButtonAction = () => {
+  fetch('http://localhost:8000/api/login/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({'username': 'johndoe', 'password': 'testpassword'})
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
 }
