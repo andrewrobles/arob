08f5ce034fac74c33c440c52d51bc28b1bf426ec
andrewrobles <andrewroblesdev@gmail.com>
2022-03-12 10:57:42 -0800

    Remove uppercase

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -4,7 +4,7 @@ export const Menu = (props) => {
     return <div className={`card`} >
         <ul className={`list-group list-group-flush`}>
             {props.items.map(item => <li className={`list-group-item`}>
-                    <h5 className={`card-title`}>{item.name.toUpperCase()}</h5>
+                    <h5 className={`card-title`}>{item.name}</h5>
                     <p className={`card-text`}>{item.ingredients}</p>
                 </li>
             )}
