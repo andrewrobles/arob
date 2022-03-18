a8c8fa45b193087c1e84d14a1692939db20d367e
andrewrobles <andrewroblesdev@gmail.com>
2022-03-17 20:55:59 -0700

    Reorder tests

--- server/api/tests.py
+++ server/api/tests.py
@@ -3,47 +3,6 @@ from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 from .models import Item, Ingredient, Extra
 
-class HelloWorldTestCase(TestCase):
-    def test_get_hello_world(self):
-        factory = APIClient()
-        actual = factory.get('/helloworld/', format='json').data
-        expected = {'message': 'Hello World!'}
-
-        self.assertEqual(actual, expected)
-
-        
-class AuthTestCase(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.credentials = {'username': 'johndoe', 'password': 'testpassword'}
-
-    def test_incorrect_login(self):
-        response = self.client.post('/api/login/', self.credentials, format='json')
-        self.assertNotEqual(list(response.data.keys()), ['token'])
-
-    def test_correct_login(self):
-        user = User.objects.create_user(**self.credentials)
-        response = self.client.post('/api/login/', self.credentials, format='json')
-        self.assertEqual(list(response.data.keys()), ['token'])
-
-    def test_sign_up(self):
-        self.client.post('/api/signup/', self.credentials, format='json')
-        response = self.client.post('/api/login/', self.credentials, format='json')
-        self.assertEqual(list(response.data.keys()), ['token'])
-
-    def test_log_out(self):
-        self.client.post('/api/signup/', self.credentials, format='json')
-        first_token = self.client.post('/api/login/', self.credentials, format='json').data['token']
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + first_token)
-        response = self.client.get('/api/logout/', format='json')
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'token': None})
-
-        self.client.credentials(HTTP_AUTHORIZATION='')
-        response = self.client.post('/api/login/', self.credentials, format='json')
-        self.assertNotEqual(first_token, response.data['token'])
-
 
 class TestApi(TestCase):
     def setUp(self):
@@ -86,3 +45,45 @@ class TestApi(TestCase):
     #     self.client.post('/api/order/', {'id': self.item.id})
     #     self.client.delete('/api/menu/order/add/', {'id': self.item.id})
     #     self.assertEqual(len(Order.objects.all()), 0)
+
+
+class HelloWorldTestCase(TestCase):
+    def test_get_hello_world(self):
+        factory = APIClient()
+        actual = factory.get('/helloworld/', format='json').data
+        expected = {'message': 'Hello World!'}
+
+        self.assertEqual(actual, expected)
+
+        
+class AuthTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.credentials = {'username': 'johndoe', 'password': 'testpassword'}
+
+    def test_incorrect_login(self):
+        response = self.client.post('/api/login/', self.credentials, format='json')
+        self.assertNotEqual(list(response.data.keys()), ['token'])
+
+    def test_correct_login(self):
+        user = User.objects.create_user(**self.credentials)
+        response = self.client.post('/api/login/', self.credentials, format='json')
+        self.assertEqual(list(response.data.keys()), ['token'])
+
+    def test_sign_up(self):
+        self.client.post('/api/signup/', self.credentials, format='json')
+        response = self.client.post('/api/login/', self.credentials, format='json')
+        self.assertEqual(list(response.data.keys()), ['token'])
+
+    def test_log_out(self):
+        self.client.post('/api/signup/', self.credentials, format='json')
+        first_token = self.client.post('/api/login/', self.credentials, format='json').data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + first_token)
+        response = self.client.get('/api/logout/', format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'token': None})
+
+        self.client.credentials(HTTP_AUTHORIZATION='')
+        response = self.client.post('/api/login/', self.credentials, format='json')
+        self.assertNotEqual(first_token, response.data['token'])
