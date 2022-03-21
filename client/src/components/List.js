6feeeef213559f4085d6c1ad5cb8a7eca896e989
Andrew Robles <andrewrobles@verizon.net>
2022-03-21 07:52:52 -0700

    Fix div shifting issue

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,20 +1,23 @@
-import { useState } from 'react' 
 import { Item } from './Item'
 
 export const List = (props) => {    
-    return <div className={`card mt-5`} >
-    <ul className={`list-group list-group-flush`}>
-        {props.items.map((item, index) => <li className={`list-group-item`}>
-                <Item data={item}/>
-                <div class="d-flex justify-content-end mb-1">
-                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                        <div class="btn-group" role="group" aria-label="Second group">
-                            <button onClick={() => props.buttonAction(index)} type="button" class="btn btn-light border">{props.buttonText}</button>
-                        </div>
-                    </div>       
-                </div>  
-            </li>
-        )}
-    </ul>
-</div>
+    return <div style={{minHeight: '100vh'}}>
+            <div className={`card mt-5`}>
+            <ul className={`list-group list-group-flush`}>
+                {props.items.map((item, index) => <li className={`list-group-item`}>
+                        <Item data={item}/>
+                        <div class="d-flex justify-content-end mb-1">
+                            <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                                <div class="btn-group" role="group" aria-label="Second group">
+                                    <button onClick={() => props.buttonAction(index)} type="button" class="btn btn-light border">{props.buttonText}</button>
+                                </div>
+                            </div>       
+                        </div>  
+                    </li>
+                )}
+            </ul>
+        </div>
+    </div>
+    
+
 }
