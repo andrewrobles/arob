af66fc6390a1027f05a580e67b358d47f1942e3d
andrewrobles <andrewroblesdev@gmail.com>
2022-04-01 08:01:21 -0700

    Use prod url for fetching menu

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,8 @@ function App() {
     getMenu()
   }, [])
 
-  const sendOrder = (order) => {
-    fetch('http://localhost:3000/order', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(order)
-    }).then(response => response.json())
-      .then(data => console.log(data))
-  }
-
   const getMenu = () => {
-    fetch('http://localhost:8000/api/menu/', {
+    fetch('https://andrewrobles.pythonanywhere.com/menu/', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
