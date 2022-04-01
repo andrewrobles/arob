1c073cf735b2105b58ef274b337149e08b524251
andrewrobles <andrewroblesdev@gmail.com>
2022-03-31 20:06:00 -0700

    Update setup script

--- server/setup.py
+++ server/setup.py
@@ -1,17 +1,7 @@
-from api.models import Item, Ingredient, Extra
+from api.models import Item
 
 from menu import menu
 
-if Item.objects.count() == 0 and Ingredient.objects.count() == 0 and Extra.objects.count() == 0:
-    for extra_data in menu['extras']:
-        Extra.objects.create(name=extra_data['name'].title(), price=extra_data['price'])
-
-    for items in menu['items']:
-        item = Item.objects.create(name=items['name'].title(), price=items['price'])
-        for ingredient_data in items['ingredients']:
-            try:
-                ingredient = Ingredient.objects.get(name=ingredient_data.title())
-            except Ingredient.DoesNotExist:
-                ingredient = Ingredient.objects.create(name=ingredient_data.title())
-            item.ingredients.add(ingredient)
-        item.save()
+if Item.objects.count() == 0:
+    for item in menu:
+        Item.objects.create(name=item['name'].title(), price=item['price'], description=item['description'])
