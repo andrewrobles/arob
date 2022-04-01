393cbe250accd892c3f7b78005fbeb0e62ec852e
andrewrobles <andrewroblesdev@gmail.com>
2022-03-31 19:12:34 -0700

    Remove excess components

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,43 +1,27 @@
-import {useState} from 'react'
-import { List } from '../components/List'
-import { Checkout } from '../components/Checkout'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Menu = (props) => {
-    const [state, setState] = useState({
-        pageIndex: 0,
-        order: []
-    })
+    return <List items={props.items}/>
+}
 
-    const addItem = (index) => {
-        const item = props.items[index]
-        setState({
-            ...state,
-            order: state.order.concat(item)
-        })
-    }
-
-    const removeItem = (index) => {
-        state.order.splice(index, 1)
-        setState({
-            ...state,
-            order: state.order
-        })
-    }
-
+const List = (props) => {    
     return <div>
-        <nav className={`navbar fixed-top navbar-expand-lg navbar-light bg-light`}>
-            <div className="mx-3">
-                <h2><strong>Order</strong></h2>
-            </div>
-        </nav>
-        <div className={`mt-5`} >
-            <List items={props.items} buttonText={"Add to order"} buttonAction={addItem}/>
+            <div className={`card`}>
+            <ul className={`list-group list-group-flush`}>
+                {props.items.map((item, index) => <li key={index} className={`list-group-item`}>
+                        <Item data={item}/>
+                    </li>
+                )}
+            </ul>
         </div>
-        {state.order.length > 0 &&  <Checkout items={state.order} buttonAction={() => alert('Button press')}
-        listButtonText={'Remove'} listButtonAction={removeItem}
-        />}
-       
+    </div>
+}
+
+const Item = (props) => {
+    return <div>
+        <strong>{props.data.name}</strong>
+        <div>
+        {props.data.ingredients.join(', ')}
+        </div>
     </div>
 }
